fb1b7c753a35f7b88269b730d768f3a491f97c33
38takumi <saitout485@gmail.com>
2021-08-01 14:53:38 +0000

    初回

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
+  has_many :books, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
+
+  validates :introduction, length: { maximum: 50 }
+
+#プロフィール画像用 deviseに入ってるかも
+  attachment :profile_image
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'home#top'
+  devise_for :users
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
+  get 'home/about' => 'home#about'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
