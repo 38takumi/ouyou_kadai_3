fdf7b8970cb65609a7a7e9ff9a75a0dfbf708254
38takumi <saitout485@gmail.com>
2021-08-06 15:57:24 +0000

    いいね機能直し中

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,11 @@ class Book < ApplicationRecord
  belongs_to :user
  has_many :book_comments, dependent: :destroy
  has_many :favorites, dependent: :destroy
-  def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
-  end
+ def favorited_by?(user)
+   # いいねを押したuser.idを全て取得、条件に当てはまるかどうか
+   favorites.where(user_id: user.id).exists?
+   # favorites.exists?(user_id: user.id)
+ end
  
  
  validates :title, presence: true
