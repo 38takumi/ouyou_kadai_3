2ce101fa9f5a679f8b3a1acf88ba33e090edaf05
38takumi <saitout485@gmail.com>
2021-08-07 06:25:12 +0000

    応用課題3完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,15 +9,16 @@ def create
   favorite = @book.favorites.new(user_id: current_user.id)
   favorite.save
   # book詳細ページ、開いていたbookの
-  redirect_to book_path(@book.id)
-  # redirect_to request.referer
+  # redirect_to book_path(@book.id)
+  redirect_to request.referer
 end
 
 def destroy
   @book = Book.find(params[:book_id])
   favorite = @book.favorites.find_by(user_id: current_user.id)
   favorite.destroy
-  redirect_to book_path(@book.id)  
+  # redirect_to book_path(@book.id) 
+  redirect_to request.referer
 end
 
 end
